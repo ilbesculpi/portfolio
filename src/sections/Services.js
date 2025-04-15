4498c8a7a24a3ef6d2484083a8f9a227ce65e748
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2025-04-15 11:54:11 -0400

    Template sections

---
+++ src/sections/Services.js
@@ -0,0 +1,90 @@
+
+function Services() {
+    return (
+        <div className="container-fluid" id="pigraService">
+            <div className="container">
+                <div className="row g-0">
+                    <div className="col-lg-3">
+                        <div className="service-header h-100 bg-dark pt-6 pe-6 pb-6">
+                            <div className="text-start d-flex flex-column justify-content-center wow fadeInUp" data-wow-delay="0.1s">
+                                <p className="text-white sub-title">My Services</p>
+                                <h1 className="display-6 text-white mb-0">What I do for you</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-9">
+                        <div className="service-content h-100 pt-6 ps-6 pb-6">
+                            <div className="row g-4">
+                                <div className="col-12">
+                                    <div className="service-item p-4 wow fadeInUp" data-wow-delay="0.1s">
+                                        <div className="row g-4 align-items-center">
+                                            <div className="col-xl-2">
+                                                <i className="fas fa-bezier-curve fa-3x"></i>
+                                            </div>
+                                            <div className="col-xl-3">
+                                                <a href="#" className="h4 mb-0">Creative Design</a>
+                                            </div>
+                                            <div className="col-xl-4">
+                                                <p className="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laboriosam repudiandae
+                                                </p>
+                                            </div>
+                                            <div className="col-xl-3">
+                                                <p><i className="fa fa-check me-2"></i> UI/UX Design</p>
+                                                <p><i className="fa fa-check me-2"></i> Research</p>
+                                                <p className="mb-0"><i className="fa fa-check me-2"></i> Mobile App</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12">
+                                    <div className="service-item p-4 wow fadeInUp" data-wow-delay="0.3s">
+                                        <div className="row g-4 align-items-center">
+                                            <div className="col-xl-2">
+                                                <i className="fas fa-laptop-code fa-3x"></i>
+                                            </div>
+                                            <div className="col-xl-3">
+                                                <a href="#" className="h4 mb-0">Web Developm</a>
+                                            </div>
+                                            <div className="col-xl-4">
+                                                <p className="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laboriosam repudiandae
+                                                </p>
+                                            </div>
+                                            <div className="col-xl-3">
+                                                <p><i className="fa fa-check me-2"></i> UI/UX Design</p>
+                                                <p><i className="fa fa-check me-2"></i> Research</p>
+                                                <p className="mb-0"><i className="fa fa-check me-2"></i> Mobile App</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12">
+                                    <div className="service-item p-4 wow fadeInUp" data-wow-delay="0.5s">
+                                        <div className="row g-4 align-items-center">
+                                            <div className="col-xl-2">
+                                                <i className="fab fa-app-store fa-3x"></i>
+                                            </div>
+                                            <div className="col-xl-3">
+                                                <a href="#" className="h4 mb-0">Mobile Application</a>
+                                            </div>
+                                            <div className="col-xl-4">
+                                                <p className="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis laboriosam repudiandae
+                                                </p>
+                                            </div>
+                                            <div className="col-xl-3">
+                                                <p><i className="fa fa-check me-2"></i> UI/UX Design</p>
+                                                <p><i className="fa fa-check me-2"></i> Research</p>
+                                                <p className="mb-0"><i className="fa fa-check me-2"></i> Mobile App</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Services;
